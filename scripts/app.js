e118ecd1b15fcbfbd973c55f868fc440685ae254
Frontend-Umidjon <umidjonkengashev6@gamil.com>
2024-12-20 05:10:52 +0500

    uy ishi

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,42 @@
-const BASE_URL = 'http://dummyjson.com';
+const BASE_URL = "https://dummyjson.com";
+const wrapperEl = document.querySelector(".products__list");
 
-async function fetchData(endpoint) {
-  const response = await fetch(`${BASE_URL}/${endpoint}`);
-  
+const fetchProducts = async (endpoint) => {
+  const response = await fetch(`${BASE_URL}${endpoint}`);
+  response
+    .json()
+    .then((data) => {
+      createCards(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error); 
+    })
+    .finally(() => {
+      console.log("Request completed");
+    });
+};
+window.onload = fetchProducts("/products?limit=8");
 
-}
+const createCards = (data) => {
+  data.products.forEach((product) => {
+    const card = document.createElement("div");
+    card.classList.add("products__item");
+    card.innerHTML = `
+        <div class="products__img">
+                        <img src="${product.thumbnail}" alt="" data-id="${product.id}">
+                    </div>
+                    <div class="products__content">
+                        <h3 class="products__heading">${product.title}</h3>
+                        <p class="products__price">${product.price}$</p>
+                    </div>
+    `;
+    wrapperEl.appendChild(card);
+  });
+};
 
-fetchData('products');
-
+wrapperEl.addEventListener("click", (e) => {
+  if (e.target.closest(".products__img")) {
+      open(`./pages/details.html?id=${e.target.dataset.id}`, "_self");
+      console.log(e.target.dataset.id);
+  }
+});
